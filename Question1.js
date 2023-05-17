5f44427ce1b6daa45f232e7942e44f4671a45d85
Nishu Singhal <104061519+NishuSinghal@users.noreply.github.com>
2023-05-18 00:59:18 +0530

    Update Question1.js

--- Question1.js
+++ Question1.js
@@ -6,17 +6,17 @@ const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config(); // Load environment variables from .env file
 
 async function findDocuments() {
-  const uri = process.env.MONGODB_URI; // Use environment variable for MongoDB connection string
+  const uri = process.env.MONGODB_URI; 
   const client = new MongoClient(uri);
 
   try {
     await client.connect();
 
     const database = client.db(process.env.DB_NAME); // Use environment variable for the database name
-    const collection = database.collection(process.env.COLLECTION_NAME); // Use environment variable for the collection name
+    const collection = database.collection(process.env.COLLECTION_NAME); 
 
-    const fieldName = process.env.FIELD_NAME; // Use environment variable for the field name
-    const value = 10; // Replace 10 with the specific value you want to compare against
+    const fieldName = process.env.FIELD_NAME; 
+    const value = 10; 
 
     const query = { [fieldName]: { $gt: value } };
     const result = await collection.find(query).toArray();
